b26bc55a97b3cbd82665932cb9a305bf285c77f0
amha1717snowflake <133920103+amha1717snowflake@users.noreply.github.com>
2023-05-17 17:39:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
 streamlit.dataframe(my_fruit_list)
